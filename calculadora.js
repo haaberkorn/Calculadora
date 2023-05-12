07c50e6ea74a327c556f39579d2decc4773c74eb
haaberkorn <keynersnack@outlook.com>
2023-05-12 00:08:47 -0800

    actualizacion

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,4 @@
+/* Operadores */
 let botonSumar = document.getElementById("botonSumar");
 let botonRestar = document.getElementById("botonRestar");
 let botonMultiplicar = document.getElementById("botonMultiplicar");
@@ -5,3 +6,23 @@ let botonDividir = document.getElementById("botonDividir");
 let botonIgual = document.getElementById("botonIgual");
 let botonClear = document.getElementById("botonClear");
 let operacionInput = document.getElementById("operacionInput");
+
+/* Numeros */
+let numeroUno = document.getElementById("numeroUno");
+let numeroDos = document.getElementById("numeroDos");
+
+const calculadora = () =>{
+    numeroUno.innerText = "1";
+    numeroDos.innerText = "2"; 
+    botonSumar = "+";  
+    botonSumar.onclick = () =>{
+        if(botonSumar = true){
+            
+        }else{
+            console.log("el boton es false");
+        }
+    }
+    console.log(`El resultado del boton fue ${eval.botonSumar}`)
+    
+}
+botonIgual.addEventListener("click", calculadora)
