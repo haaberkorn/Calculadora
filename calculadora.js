7ab0d063677b1fc610dbc5557506073e3771c2c9
haaberkorn <keynersnack@outlook.com>
2023-05-11 22:43:30 -0800

    Initial commit

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+let botonSumar = document.getElementById("botonSumar");
+let botonRestar = document.getElementById("botonRestar");
+let botonMultiplicar = document.getElementById("botonMultiplicar");
+let botonDividir = document.getElementById("botonDividir");
+let botonIgual = document.getElementById("botonIgual");
+let botonClear = document.getElementById("botonClear");
+let operacionInput = document.getElementById("operacionInput");
